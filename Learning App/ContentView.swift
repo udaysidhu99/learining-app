c9bd013440a5b6a52bcccfa3e90b6c339aebffff
Uday <udaysidhu1@outlook.com>
2023-01-27 19:10:35 +0530

    Test Commit

--- Learning App/ContentView.swift
+++ Learning App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("First Commit")
+            Text("First Commit!")
         }
         .padding()
     }
