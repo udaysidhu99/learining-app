4cfe00398fa28363918faed474e3f79f60e622c2
Uday <udaysidhu1@outlook.com>
2023-01-27 19:06:59 +0530

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Uday Sidhu on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
