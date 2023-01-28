9222c33bf8802c8722476b9b5374252c76e158d0
Uday <udaysidhu1@outlook.com>
2023-01-28 15:37:34 +0530

    add HomeCardView flesh out HomeView

--- Learning App/View/HomeView.swift
+++ Learning App/View/HomeView.swift
@@ -10,18 +10,31 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("First Commit!")
+        NavigationView{
+            VStack(alignment: .leading){
+                Text("What are we learning today?").padding(.leading, 20)
+                ScrollView{
+                    LazyVStack{
+                        ForEach(model.modules){ i in
+                            VStack(spacing: 20){
+                                
+                                HomeViewCard(image: i.content.image, title:"Learn \(i.category)" , descirption:i.content.description, count: "\(i.content.lessons.count) Lessons", time: i.content.time)
+                                
+                                HomeViewCard(image: i.test.image, title:"\(i.category) Test" , descirption:i.test.description, count: "\(i.test.questions.count) Questions", time: i.test.time)
+                            }
+                            
+                        }
+                    }
+                }
+                .padding()
+            }.navigationTitle("Get Started")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        let model = ContentModel()
+        HomeView().environmentObject(model)
     }
 }
---
+++ Learning App/View/HomeViewCard.swift
@@ -0,0 +1,50 @@
+//
+//  HomeViewCard.swift
+//  Learning App
+//
+//  Created by Uday Sidhu on 28/01/23.
+//
+
+import SwiftUI
+
+struct HomeViewCard: View {
+    var image: String
+    var title: String
+    var descirption: String
+    var count: String
+    var time: String
+    var body: some View {
+            ZStack{            Rectangle().foregroundColor(.white).cornerRadius(10).shadow(radius: 10).aspectRatio(CGSize(width: 335, height: 175),contentMode: .fit).padding(.horizontal)
+                
+                HStack {
+                    Image(image).resizable().frame(width:116, height: 116).clipShape(Circle()).padding(.leading,25)
+                    VStack(alignment: .leading){
+                        Text(title)
+                            .font(.title2)
+                            .fontWeight(.semibold).padding(.bottom, 2.0)
+                        Text(descirption).padding(.trailing,20).font(.system(size: 15))
+                        HStack{
+                            Image(systemName: "text.book.closed").resizable().frame(width: 15, height: 15)
+                            Text(count)
+                                .font(.system(size: 11))
+                            Image(systemName: "clock")
+                            Text(time)
+                                .font(.system(size: 11))
+                            
+                        }.padding(.trailing,25)
+                    
+                    }
+                }
+                
+            }
+        
+        
+    }
+}
+
+
+struct HomeViewCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewCard(image: "swift", title:"Learn Swift", descirption: "some description this is a significantly longer description to test out how the view looks", count: "10 Questions", time: "10 Minutes")
+    }
+}
