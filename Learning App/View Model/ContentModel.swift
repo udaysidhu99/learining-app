6cf9da6a99360c84e3dd590c3fda856702751d1a
Uday <udaysidhu1@outlook.com>
2023-01-28 10:15:40 +0530

    add assets add data files parse json and style files add models

--- Learning App/View Model/ContentModel.swift
+++ Learning App/View Model/ContentModel.swift
@@ -9,4 +9,38 @@ import Foundation
 
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    
+    init(){
+        getLocalData()
+    }
+    func getLocalData(){
+        
+        
+        let url = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do{
+            let data = try Data(contentsOf: url!)
+            let decoder = JSONDecoder()
+            self.modules = try  decoder.decode([Module].self, from: data)
+            
+            
+        }
+        catch{
+            print("error fetching data")
+        }
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        do{
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }
+        
+        catch{
+            print("error parsing style data")
+        }
+        
+    }
+    
 }
